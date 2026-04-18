338d01d7757ba1ddfed4f26b0c4ef1e07f0e0018
anshi 10 <anshi.sabesan@datawizpro.com>
2026-04-18 11:00:26 -0400

    added logic for calc

--- source/calculator.py
+++ source/calculator.py
@@ -27,7 +27,18 @@ def mult(num1, num2):
 
 # Write the logic for the calculator  TODO
 # Use the functions 
-num1 = int(input(" Give me 1 number  "))
-num2 = int(input(" Give me another number  "))
-operation = input(" Tell me what operation I have to do on these 2 numbers")
+num1 = int(input(" Give me 1 number: "))
+num2 = int(input(" Give me another number: "))
+operation = input(" Tell me what operation I have to do on these 2 numbers: ")
 
+if operation == "add" :
+    print(add(num1, num2))
+
+elif operation == "sub" :
+    print(sub(num1, num2))
+
+elif operation  == "div" :
+    print(div(num1, num2))
+
+else : 
+    print(mult(num1, num2))
