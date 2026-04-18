5a5485c5c5d1d9eb48f88521f74d448abb6b5958
Abinash <abinash.nagendran@datawizpro.com>
2026-04-18 10:13:39 -0400

    starter code

---
+++ source/calculator.py
@@ -0,0 +1,24 @@
+# ------------------------------------------
+
+
+# HELPER FUNCTIONS 
+
+
+
+# Add functions TODO
+# Subtraction function TODO
+
+# Division function TODO
+
+# Multiplication function TODO
+
+
+
+
+# ------------------------------------------
+
+# Ask for 2 numbers and operator TODO
+
+
+# Write the logic for the calculator  TODO
+# Use the functions 
